bada22f4c1fa422a141be84b81e4f4731a97d0eb
Raviteja Kalipatnapu <user@users-MacBook-Pro.local>
2021-05-28 00:04:05 +0530

    add all in main.tf file

--- main.tf
+++ main.tf
@@ -1,15 +1,36 @@
 provider "aws" {
-  region = var.region
+  region = "us-east-1"
 }
 
-module "elasticsearch" {
-  source = "cloudposse/elasticsearch/aws"
-  namespace               = "eg"
-  stage                   = "dev"
-  name                    = "es"
-  zone_awareness_enabled  = "true"
-  elasticsearch_version   = 7.10
-  instance_type           = "t2.small.elasticsearch"
-  instance_count          = 1
-  ebs_volume_size         = 10
+module "aws_es" {
+
+  source  = "lgallard/elasticsearch/aws"
+
+  domain_name           = "elasticsearch-publicaws"
+  elasticsearch_version = "7.10"
+
+  cluster_config = {
+    dedicated_master_enabled = false
+    instance_count           = 1
+    instance_type            = "t2.small.elasticsearch"
+    zone_awareness_enabled   = false
+    availability_zone_count  = 0
+  }
+
+  ebs_options = {
+    ebs_enabled = "true"
+    volume_size = "10"
+  }
+
+  log_publishing_options = {
+    enabled                  = false
+    log_type                 = "INDEX_SLOW_LOGS"
+  }
+
+  encrypt_at_rest = {
+    enabled    = false
+  }
+
+  node_to_node_encryption_enabled  = true
+
 }
